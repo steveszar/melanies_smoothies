2dab9fb8bc82fd8fa8675150222bf2727e9b6fb8
steveszar <stevenszarka@gmail.com>
2024-06-03 17:31:09 -0600

    Update streamlit_app.py
    display JSON response from API

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,7 @@ if ingredients_list:
 # New sectin to display fruityvice nutrition information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
 
 
 
