e420557d03d9775bcb7ea63c529eab0760fdc800
steveszar <stevenszarka@gmail.com>
2024-06-03 17:32:09 -0600

    Update streamlit_app.py
    JSON to DF

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,8 +59,8 @@ if ingredients_list:
 # New sectin to display fruityvice nutrition information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
-
+#st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_reponse.json(), use_container_width=True)
 
 
 
