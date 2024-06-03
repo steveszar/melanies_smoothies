e566cb611c265622d5754bd9eadc049947b328e7
steveszar <stevenszarka@gmail.com>
2024-06-03 16:08:57 -0600

    Create streamlit_app.py
    copied from my custom smoothies SiS app (created in Snowflake)

---
+++ streamlit_app.py
@@ -0,0 +1,48 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+
+from snowflake.snowpark.functions import col, when_matched
+
+# Write directly to the app
+st.title(":cup_with_straw: Pending Smoothie Orders :cup_with_straw:")
+st.write(
+    """Orders that need to be filled.
+    """
+)
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.orders").filter(col("Order_Filled")==0).collect()
+#st.dataframe(data=my_dataframe, use_container_width=True)
+
+
+if my_dataframe:
+
+    editable_df = st.experimental_data_editor(my_dataframe)
+    
+    submitted = st.button('Submit')
+    
+    if submitted:
+       
+        try:
+            og_dataset = session.table("smoothies.public.orders")
+            edited_dataset = session.create_dataframe(editable_df)
+            og_dataset.merge(edited_dataset
+                         , (og_dataset['order_uid'] == edited_dataset['order_uid'])
+                         , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+                        )
+        
+            st.success('Someone clicked the button', icon="✅")
+    
+        except:
+            st.write('Something went wrong.')
+
+else:
+    st.success('There are no pending orders right now', icon="✅")
+
+
+
+
+
+
+
