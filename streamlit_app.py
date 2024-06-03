c190f21bd89969e00ab550cb12dd7523652c87d7
steveszar <stevenszarka@gmail.com>
2024-06-03 17:33:56 -0600

    Update streamlit_app.py
    typo correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,11 +56,11 @@ if ingredients_list:
         st.success('Your Smoothie is ordered,'+ name_on_order +'!', icon="✅")
 
 
-# New sectin to display fruityvice nutrition information
+# New section to display fruityvice nutrition information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_reponse.json(), use_container_width=True)
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 
 
