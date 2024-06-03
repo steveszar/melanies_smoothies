7170e9595d189b9557168eeea164c4c200b26a8c
steveszar <stevenszarka@gmail.com>
2024-06-03 17:37:16 -0600

    Update streamlit_app.py
    tab error fix 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,6 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
 	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 	fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
