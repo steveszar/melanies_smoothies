f0333861bedcb28fd1ab518c50a893761eb92647
steveszar <stevenszarka@gmail.com>
2024-06-03 16:42:02 -0600

    Update streamlit_app.py
    remove space st. connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ st.write('The name on your Smoothie will be:', name_on_order)
 #
 #st.write("Your favorite fruit is:", option)
 
-cnx = st. connection("snowflake")
+cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
